85eccabcbd01e84dfd14efc571bf80d12279509c
Hannah <hgarcia.cymru@gmail.com>
2017-10-30 20:49:44 +0000

    Updated Allowed hosts on settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'trcja$n^uh(1m$vh(eox@9vfsfayd((85q56%9m&-*ht_^o^c3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hgarciacymru.pythonanywhere.com']
 
 
 # Application definition
